15cb6823f6095c4f33278b6fa97dd0bcb396a9f9
mariapavezi <maria.luisa.pavezi.08@gmail.com>
2025-10-21 17:14:24 -0300

    adicionei repositorio livro

---
+++ biblioteca/src/main/java/com/mlp/biblioteca/repositories/LivroRepository.java
@@ -0,0 +1,7 @@
+package com.mlp.biblioteca.repositories;
+
+import com.mlp.biblioteca.entities.LivroEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroRepository extends JpaRepository <LivroEntity, Long> {
+}
